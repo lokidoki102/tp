fb9b9f415380964e75b75c96839221c9168eea9f
redcolorbicycle <71276799+redcolorbicycle@users.noreply.github.com>
2024-04-02 20:36:12 +0800

    Update UniqueHouseList.java

--- src/main/java/seedu/address/model/house/UniqueHouseList.java
+++ src/main/java/seedu/address/model/house/UniqueHouseList.java
@@ -39,9 +39,9 @@ public class UniqueHouseList implements Iterable<House> {
             int cutOffPriceOfHouse = toCheck.toString().toLowerCase().lastIndexOf("price");
             int cutOffPriceOfHouseInList = h.toString().toLowerCase().lastIndexOf("price");
 
-            String firstAddress = toCheck.toString().substring(checkAddressOfHouse + "street".length(), 
+            String firstAddress = toCheck.toString().substring(checkAddressOfHouse + "street".length(),
                                                                cutOffPriceOfHouse).toLowerCase();
-            String secondAddress = h.toString().substring(checkAddressOfHouseInList + "street".length(), 
+            String secondAddress = h.toString().substring(checkAddressOfHouseInList + "street".length(),
                                                           cutOffPriceOfHouseInList).toLowerCase();
             if (firstAddress.equals(secondAddress)) {
                 return true;
