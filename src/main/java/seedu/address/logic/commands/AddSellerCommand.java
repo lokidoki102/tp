ceb96aaf941d28d45f1449c9dc3e6a5a75fe39fd
redcolorbicycle <liuzhengyang10@gmail.com>
2024-04-02 19:21:49 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/commands/AddSellerCommand.java
+++ src/main/java/seedu/address/logic/commands/AddSellerCommand.java
@@ -19,7 +19,6 @@ import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.house.House;
-import seedu.address.model.person.Person;
 import seedu.address.model.person.Seller;
 
 /**
