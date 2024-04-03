fa903342c1714a8b198e9b02dfea828542521a6f
redcolorbicycle <liuzhengyang10@gmail.com>
2024-04-03 20:16:29 +0800

    Fix Test Case

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -54,7 +54,7 @@ public class Name implements Comparable<Name> {
         }
 
         Name otherName = (Name) other;
-        return fullName.equalsIgnoreCase(otherName.fullName);
+        return fullName.equals(otherName.fullName);
     }
 
     @Override
